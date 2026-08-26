5cfa90429bcb86211cc3307ac955881bf7d274fd
Mark Gauzza <markgauzza@gmail.com>
2026-08-26 16:49:17 -0400

    fix folder structure

--- bentley/src/bentley.DataAccess/Repositories/Interfaces/IFormValidatable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace bentley.DataAccess.Repositories.Interfaces
-{
-    public interface IFormValidatable
-    {
-        public string Subject { get; }
-        public int? Priority { get; }
-    }
-}
